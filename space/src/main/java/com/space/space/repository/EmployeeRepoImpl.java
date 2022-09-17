90a432003af12f654078138fe05379c0c8b7637e
AtulKumarIndian <atulkrindian@gmail.com>
2022-09-17 14:51:43 +0530

    added kushagra as emp

--- space/src/main/java/com/space/space/repository/EmployeeRepoImpl.java
+++ space/src/main/java/com/space/space/repository/EmployeeRepoImpl.java
@@ -19,7 +19,9 @@ public class EmployeeRepoImpl implements EmployeeRep {
 		
 		employeeDb=new HashMap<>();
 		Employee Sejal_Jaiswal= new Employee(1011,"Sejal Jaiswal", null, "VP","MB", TeamOECode.MBLE,DepartmentOECode.TLM, "Kartikeya");
-        Employee Atul_Kumar_Gupta= new Employee(1012,"Atul Kumar Gupta", Sejal_Jaiswal, "AVP","MB", TeamOECode.MBLE,DepartmentOECode.TLM, "Kartikeya");
+        Employee Atul_Kumar_Gupta= new Employee(1012,"Atul Kumar Gupta", Sejal_Jaiswal, "AVP","MB", TeamOECode.MBLE,DepartmentOECode.TLM, "Sejal Jaiswal");
+        Employee Atharva_Sohani= new Employee(1013,"Atharva Sohani", Sejal_Jaiswal, "AVP","MB", TeamOECode.MBLE,DepartmentOECode.TLM, "Bhot Sare");
+        Employee Kushagra_Upadhyay= new Employee(1013,"Kushagra Upadhyay", Sejal_Jaiswal, "AVP","MB", TeamOECode.MBLE,DepartmentOECode.TLM, "Kartik Bansal");
 		
 		
 		
