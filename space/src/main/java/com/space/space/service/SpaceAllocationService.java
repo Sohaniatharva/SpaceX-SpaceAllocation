10e7534b306b503fc633d17d5178d07e2127cda1
Atharva <sohaniathrava@gmail.com>
2022-09-19 01:00:36 +0530

    latest code

--- space/src/main/java/com/space/space/service/SpaceAllocationService.java
+++ space/src/main/java/com/space/space/service/SpaceAllocationService.java
@@ -1,11 +1,12 @@
 package com.space.space.service;
 
 import com.space.space.model.DepartmentSeats;
+import com.space.space.model.Space;
 import com.space.space.model.SpaceAllocation;
 
 import java.util.Date;
 
 public interface SpaceAllocationService {
-    boolean allocateSpace(SpaceAllocation spaceAllocation);
+    boolean allocateSpace(Space space);
     DepartmentSeats getAllSeats(int empId);
 }
